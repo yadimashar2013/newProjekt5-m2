e4ecc5f3dbf9e58f24bac4ae74eba685c05d52c6
yadimashar2013 <162845121+yadimashar2013@users.noreply.github.com>
2024-03-19 08:48:42 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+def print_params(params):
+    print('Параметры двигателя:', params * 2)
+
+
+print_params('Тип двигателя')
+print_params('Расход топлива')
+print_params('Марка топлива')
+print_params('Расход масла')
